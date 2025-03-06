b7c5b024346626be3b08b98d45b70bf6bf13d305
pigeatgarlic <huyhoangdo0205@gmail.com>
2025-03-06 06:11:09 +0000

    ip route set default

--- router.node.sh
+++ router.node.sh
@@ -8,5 +8,5 @@ ip addr flush dev $apiIF
 ip addr add 10.30.30.1/24 dev $apiIF
 ip link set dev $apiIF mtu 9000
 
-ip route add default via 10.30.30.0 via 10.30.30.1
+ip route add default 10.30.30.0 via 10.30.30.1
 ip addr flush dev $publicIF
